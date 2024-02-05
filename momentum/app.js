b85f3c664135374a8f5593009dd22c89b4574398
yekim0325 <yeeun940325@gmail.com>
2024-02-05 21:29:05 +0900

    app.js 수정 - Array, Objects, function 추가

--- momentum/app.js
+++ momentum/app.js
@@ -36,3 +36,96 @@ console.log(fined);
 
 let something;          // 변수는 있지만 정의하지 않음. 공간은 있는데 값이 존재하지 않음
 console.log(something); // undefined
+
+// ===============================================================
+
+// 05. Array (배열)
+const daysOfWeek = ["mon", "tue", "wed", "thu", "fri", "sat"];
+
+// Get Item from Array
+console.log(daysOfWeek);
+
+// Add one more day to the Array
+daysOfWeek.push("sun");
+
+console.log(daysOfWeek);
+
+const toBuy = ["potato", "tomato", "pizza"];
+toBuy.push("kimbab");
+console.log(toBuy[2]);
+
+// ===============================================================
+
+// 06. Objects
+
+const playerName = "yeeun";
+const playerPoints = 121212;
+const playerHandsome = true;
+const playerFat = "little bit";
+
+// player Array
+// const player = ["yeeun", 121212, true, "little bit"];   
+// => Array는 각각의 항목이 무슨 의미인지 알려주고 있지 않아 적합하지 않음
+
+const player = {
+    name : "yeeun",
+    points : 10,
+    handsome : true,
+    fat : "little bit",
+};
+console.log(player);
+console.log(player.name);
+console.log(player["name"]);
+
+player.handsome = false;
+console.log(player);
+
+// const는 수정할 수 없다고 했는데??  
+// const 안의 무언가를 수정하는 것은 상관없다. but const로 선언된 Object를 수정하려고 하면 오류가 발생!
+
+// const안에 무언가를 추가하게 되면 어떻게 될까?
+player.lastName = "potato";
+console.log(player);
+
+// * Object에서 값 가져오기
+console.log(player.points);
+
+// * Object 값 수정하기
+console.log(player.points = player.points + 15);
+
+// * Obeject 값 추가하기
+player.color = "red";
+console.log(player);
+
+// ===============================================================
+
+// 07. function : 반복해서 사용할 수 있는 코드 조각
+ 
+function sayHello(nameOfPerson, age){
+    console.log("Hello!! my name is " + nameOfPerson + " and I'm " + age);
+}
+
+sayHello("nico", 38);
+sayHello("dal", 12);
+sayHello("yeeun", 29);
+
+function plus(a, b){
+    // console.log(a+b); // NaN (Not a Number, 숫자가 아님)
+    console.log(a+b);
+}
+function divide(a,b){
+    console.log(a/b);
+}
+
+plus(8, 60);
+divide(98,20);
+
+const player2 = {
+    name : "nico",
+    sayHello : function(otherPersonName){
+        console.log("hello!!! " + otherPersonName + " nice to meet you!");
+    },
+};
+
+console.log(player2.name);
+player2.sayHello("lynn");
