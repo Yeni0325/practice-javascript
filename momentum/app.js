75675080f394701a863afda2eae85286001b5612
bk08 <bk8@kotrabk.com>
2024-02-05 16:24:45 +0900

    JS repository 생성 및 바닐라js 공부 파일 추가

---
+++ momentum/app.js
@@ -0,0 +1,11 @@
+
+const a = 5;
+const b = 2;
+
+const myName = "yeeun";
+const veryLongVariableName = 0; // camel case
+
+console.log(a + b);
+console.log(a * b);
+console.log(a / b);
+console.log("hello " + myName);
