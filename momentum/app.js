e215b073ab1e1413c6a68b1387dfd792db9b6607
bk08 <bk8@kotrabk.com>
2024-02-05 16:56:38 +0900

    app.js 수정 1. Boolean, null, undefined 설명 추가

--- momentum/app.js
+++ momentum/app.js
@@ -1,11 +1,38 @@
 
+// 01. constant (상수) & let
+
 const a = 5;
 const b = 2;
-
-const myName = "yeeun";
+// const myName = "yeeun";
+let myName = "yeeun";
 const veryLongVariableName = 0; // camel case
 
 console.log(a + b);
 console.log(a * b);
 console.log(a / b);
 console.log("hello " + myName);
+
+myName = "kimYeeun";    //  Assignment to constant variable.
+
+console.log("your new name is " + myName);
+
+// ===============================================================
+
+// 02. Boolean
+const amIFat = true;
+console.log(amIFat);
+
+// ===============================================================
+
+// 03. Null : 아무것도 없음
+const test = null;
+console.log(test);
+
+// ===============================================================
+
+// 04. undefined : 정의되지않음
+const fined = null;     // 값은 있는데 아무것도 아닌 것
+console.log(fined);
+
+let something;          // 변수는 있지만 정의하지 않음. 공간은 있는데 값이 존재하지 않음
+console.log(something); // undefined
