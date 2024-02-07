2efe7f253631439037ebc24a93e943d24f7766f1
yekim0325 <yeeun940325@gmail.com>
2024-02-07 23:45:54 +0900

    app.js 수정 - 유저정보 localStorage에 저장하기

--- nomadCoders/createChromeAppWithVanillaJs/momentum/2/app.js
+++ nomadCoders/createChromeAppWithVanillaJs/momentum/2/app.js
@@ -3,6 +3,10 @@ const loginInput = document.querySelector("#login-form input");
 const loginButton = document.querySelector("#login-form button");
 
 const link = document.querySelector("a");
+const greeting = document.querySelector("#greeting");
+
+const HIDDEN_CLASSNAME = "hidden";
+const USERNAME_KEY = "username";
 
 /*
 function handleButtonClick(){
@@ -19,15 +23,25 @@ loginButton.addEventListener("click", handleButtonClick);
 
 function onLoginSubmit(event){
     // Event 함수의 첫번째 인자는 무조건 현재 일어난 이벤트에 대한 정보를 담고 있음!!
-    event.preventDefault(); // 어떤 event의 기본행동이 일어나지 않도록 막음
+    //event.preventDefault(); // 어떤 event의 기본행동이 일어나지 않도록 막음
     //const username = loginInput.value;
-    console.log(loginInput.value);
+    //console.log(loginInput.value);
 
     // 브라우저는 엔터를 누를 때 새로고침 하고 form을 submit 하도록 되어있음.
     // 여기서 해야할 일은 이 기본동작이 발생하지 않도록 하는 것!
+
+    event.preventDefault();
+    loginForm.classList.add(HIDDEN_CLASSNAME);
+
+    // 로그인 시 유저이름 저장하기
+    const username = loginInput.value;
+    localStorage.setItem(USERNAME_KEY, username);
+    //greeting.innerText = "Hello " + username;
+    paintGreetings(username);  
+
+    // localStorge에 유저명이 존재할 경우, form 숨기기
 }
 
-loginForm.addEventListener("submit", onLoginSubmit);
 
 function hadleLinkClick(event){
     event.preventDefault();
@@ -35,4 +49,22 @@ function hadleLinkClick(event){
 }
 
 link.addEventListener("click", hadleLinkClick);
+
+// form을 보여주기 전, addEventListener를 하기 전, localStorage에 유저명이 존재하는지 확인 (유저 정보 확인)
+
+function paintGreetings(username){
+    greeting.innerText = `Hello ${username}`;
+    greeting.classList.remove(HIDDEN_CLASSNAME);
+}
+
+const savedUsername = localStorage.getItem(USERNAME_KEY);
+
+if(savedUsername === null){
+    // show the form
+    loginForm.classList.remove(HIDDEN_CLASSNAME);
+    loginForm.addEventListener("submit", onLoginSubmit);
+} else {
+    // show the greetings
+    paintGreetings(savedUsername);   
+}
 
