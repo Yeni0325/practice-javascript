781c6cef064a3746c96624e1523257ff9fdd21dd
yekim0325 <yeeun940325@gmail.com>
2024-02-25 19:19:40 +0900

    add calculator js

---
+++ udemy/02/assets/scripts/app.js
@@ -0,0 +1,67 @@
+const defaultResult = 0;
+let currentResult = defaultResult;
+let logEntries = [];
+
+// Gets input from input field
+function getUserInput(){
+    return parseInt(userInput.value);
+}
+
+// Generates and writes calculation log
+function createAndWriteOutput(operator, resultBeforeCalc, calcNumber){
+    const calcDescription = `${resultBeforeCalc} ${operator} ${calcNumber}`;  
+    outputResult(currentResult, calcDescription); // from vender file
+}
+
+function writeToLog(operationIdentifier, prevResult, operationNumber, newResult){
+    const logEntry = {
+        operation: operationIdentifier,
+        prevResult: prevResult,
+        number: operationNumber,
+        result: newResult
+    };
+    logEntries.push(logEntry);
+    console.log(logEntries);
+}
+
+function add(){
+    const enteredNumber = getUserInput();
+    const initialResult = currentResult;
+    currentResult += enteredNumber; 
+    // currentResult = currentResult + +userInput.value; // 문자열 앞에 있는 +는 문자열을 숫자로 전환함
+    // currentResult++;
+    // currentRusult--;
+    createAndWriteOutput('+', initialResult, enteredNumber);
+    writeToLog('ADD', initialResult, enteredNumber, currentResult);
+}
+
+function subtract(){
+    const enteredNumber = getUserInput();
+    const initialResult = currentResult;
+    currentResult -= enteredNumber;
+    createAndWriteOutput('-', initialResult, enteredNumber);
+    writeToLog('SUBTRACT', initialResult, enteredNumber, currentResult); 
+}
+
+function multiply(){
+    const enteredNumber = getUserInput();
+    const initialResult = currentResult;
+    currentResult *= enteredNumber;
+
+    createAndWriteOutput('*', initialResult, enteredNumber);
+    writeToLog('MULTIPLY', initialResult, enteredNumber, currentResult);
+}
+
+function divide(){
+    const enteredNumber = getUserInput();
+    const initialResult = currentResult;
+    currentResult /= enteredNumber;
+
+    createAndWriteOutput('/', initialResult, enteredNumber);
+    writeToLog('DIVIDE', initialResult, enteredNumber, currentResult);
+}
+
+addBtn.addEventListener('click', add);
+subtractBtn.addEventListener('click', subtract);
+multiplyBtn.addEventListener('click', multiply);
+divideBtn.addEventListener('click', divide);
