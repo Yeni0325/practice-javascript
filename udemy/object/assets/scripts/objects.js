99439bdb57b1bb4c95d465465263a94b4f8c9833
bk08 <bk8@kotrabk.com>
2024-03-05 13:36:11 +0900

    Object 구조 및 기본 개념 추가

---
+++ udemy/object/assets/scripts/objects.js
@@ -0,0 +1,39 @@
+// const movieList = document.getElementById('movie-list');
+
+// movieList.style.backgroundColor = 'red';
+// movieList.style.display = 'block';
+
+// const userChosenKeyName = 'level';
+
+// let person = {
+//     'first name' : 'Max' ,
+//     age : 30 ,
+//     hobbies : ['Sports', 'Cooking'] , 
+//     [userChosenKeyName] : '...' , 
+//     greet : function(){
+//         alert('Hi there!');
+//     } , 
+//     1.5 : 'hello'
+// };
+
+// person = {
+//     name : 'Max' ,
+//     age : 30 ,
+//     hobbies : ['Sports', 'Cooking'] , 
+//     greet : function(){
+//         alert('Hi there!');
+//     } , 
+//     isAdmin : true
+// };
+
+// person.age = 33;
+// delete person.age;
+// person.age = undefined;
+// person.age = null;
+// person.isAdmin = true;
+
+// const keyName = 'first name';
+// console.log(person.keyName);    // keyName에 속해있는 프로퍼티의 값을 찾기 때문에 undefined 출력
+// console.log(person[keyName]);   // max
+// console.log(person[1.5]);
+// console.log(person);
