44957b16a680ee62d1d60704001c607b0120f050
bk08 <bk8@kotrabk.com>
2024-03-06 17:57:34 +0900

    상속, 생성자 등등 내용 추가

--- udemy/10.oop/assets/scripts/app.js
+++ udemy/10.oop/assets/scripts/app.js
@@ -1,14 +1,160 @@
 class Product {
-    title = 'DEFAULT';
-    imageUrl;
-    description;
-    price;
+    // title = 'DEFAULT';
+    // imageUrl;
+    // description;
+    // price;
 
     constructor(title, image, desc, price){
         this.title = title;
         this.imageUrl = image;
         this.description = desc;
         this.price = price;
+    }
+}
+
+class ElementAttribute {
+    constructor(attrName, attrValue){
+        this.name = attrName;
+        this.value = attrValue;
+    }
+}
+
+class Component {
+    constructor(renderHookId, shouldRender = true){
+        this.hookId = renderHookId;
+        if(shouldRender){
+            this.render();
+        }
+    }
+
+    render(){}
+
+    createRootElement(tag, cssClasses, attributes){
+        const rootElement = document.createElement(tag);
+        if(cssClasses){
+            rootElement.className = cssClasses;
+        }
+        if(attributes && attributes.length > 0){
+            for(const attr of attributes){
+                rootElement.setAttribute(attr.name, attr.value);
+            }
+        }
+        document.getElementById(this.hookId).append(rootElement);
+        return rootElement;
+    }
+}
+
+class ShoppingCart extends Component{
+    items = [];
+
+    set cartItems(value){
+        this.items = value;
+        this.totalOuput.innerHTML = `<h2>Total : \$${this.totalAmount.toFixed(2)}</h2>`;
+    }
+
+    get totalAmount(){
+        const sum = this.items.reduce((prevValue, curItem) => {
+            return prevValue + curItem.price;
+        }, 0);
+        return sum;
+    }
+
+    constructor(renderHookId){
+        super(renderHookId, false);
+        this.orderProducts = () => {
+            console.log('Ordering...');
+            console.log(this.items);
+        }
+        this.render();
+    }
+    
+    addProduct(product){
+        const updatedItems = [...this.items];
+        updatedItems.push(product);
+        this.cartItems = updatedItems;
+    }
+
+    render(){
+        const cartEl = this.createRootElement('section', 'cart');
+        cartEl.innerHTML = `
+            <h2>Total : \$${0}</h2>
+            <button>Order Now!</button>
+        `;
+        const orderButton = cartEl.querySelector('button');
+        // orderButton.addEventListener('click', this.orderProducts.bind(this));
+        // rderButton.addEventListener('click', () => this.orderProducts());
+        orderButton.addEventListener('click', this.orderProducts);
+        this.totalOuput = cartEl.querySelector('h2');
+    }
+}
+
+class ProductItem extends Component{ // 단일 상품 아이템을 렌더링
+    constructor(product, renderHookId){
+        super(renderHookId, false);
+        this.product = product;
+        this.render();
+    }
+
+    addToCart(){
+        App.addProductToCart(this.product);
+    }
+
+    render(){
+        const prodEl = this.createRootElement('li','product-item');
+        prodEl.innerHTML = `
+            <div>
+                <img src="${this.product.imageUrl}" alt="${this.product.title}">
+                <div class="product-item__content">
+                    <h2>${this.product.title}</h2>
+                    <h3>\$${this.product.price}</h3>
+                    <p>${this.product.description}</p>
+                    <button>Add to Cart</button>
+                </div>
+            </div>
+        `;
+        const addCartButton = prodEl.querySelector('button');
+        addCartButton.addEventListener('click', this.addToCart.bind(this));
+    }
+}
+
+class ProductList extends Component{
+    products = [];
+    constructor(renderHookId){
+        super(renderHookId);
+        this.fetchProducts();
+    }
+
+    fetchProducts(){
+        this.products = [
+            new Product(
+                'A Pillow', 
+                'https://www.ikea.com/kr/en/images/products/skogsfraeken-pillow-low__0970237_ph172412_s5.jpg?f=s', 
+                'A soft pillow!', 
+                19.99
+                ),
+            new Product(
+                'A Carpet', 
+                'https://m.media-amazon.com/images/I/916yp2kvMcL._AC_UF894,1000_QL80_.jpg', 
+                'A carpet which you might like - or not.', 
+                89.99
+                )
+        ];
+        this.renderProducts();
+    }
+
+    renderProducts(){
+        for(const prod of this.products){
+            new ProductItem(prod, 'prod-list'); // 생성자 안에서는 생성 중인 객체를 참조하는데 그것이 new 키워드의 역할 
+        }
+    }
+
+    render(){
+        this.createRootElement('ul', 'product-list', [
+            new ElementAttribute('id', 'prod-list')
+        ]);
+        if(this.products && this.products.length > 0){
+            this.renderProducts();
+        }
     }
 }
 
@@ -39,27 +185,32 @@ const productList = {
         // }
     ] , 
     render() {
-        const renderHook = document.getElementById('app');
-        const prodList = document.createElement('ul');
-        prodList.className = 'product-list';
-        for(const prod of this.products){
-            const prodEl = document.createElement('li');
-            prodEl.className = 'product-item';
-            prodEl.innerHTML = `
-                <div>
-                    <img src="${prod.imageUrl}" alt="${prod.title}">
-                    <div class="product-item__content">
-                        <h2>${prod.title}</h2>
-                        <h3>\$${prod.price}</h3>
-                        <p>${prod.description}</p>
-                        <button>Add to Cart</button>
-                    </div>
-                </div>
-            `;
-            prodList.append(prodEl);
-        }
-        renderHook.append(prodList);
     }    
 };
 
-productList.render();
+class Shop extends Component{
+    constructor(){
+        super();    
+    }
+
+    render(){
+        this.cart = new ShoppingCart('app');
+        new ProductList('app');
+    }
+}
+
+class App {
+    static cart;
+
+    static init(){
+        const shop = new Shop();
+        this.cart = shop.cart;
+    }
+
+    static addProductToCart(product){
+        this.cart.addProduct(product);
+    }
+}
+
+App.init();
+
